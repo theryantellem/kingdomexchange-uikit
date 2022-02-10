70dba7ac3bb1f0f072e477817e6455b484b4db18
= <=>
2022-02-10 22:33:39 +0100

    uikit

---
+++ src/widgets/Menu/config.ts
@@ -0,0 +1,73 @@
+export const links = [
+  {
+    label: "Home",
+    icon: "HomeIcon",
+    href: "/",
+  },
+  {
+    label: "Trade",
+    icon: "TradeIcon",
+    items: [
+      {
+        label: "Exchange",
+        href: "https://exchange.mozartfinance.io",
+      },
+      {
+        label: "Liquidity",
+        href: "https://exchange.mozartfinance.io/#/pool",
+      },
+    ],
+  },
+  {
+    label: "Farms",
+    icon: "FarmIcon",
+    href: "/farms",
+  },
+  {
+    label: "Pools",
+    icon: "PoolIcon",
+    href: "/syrup",
+  },
+  {
+    label: "More",
+    icon: "MoreIcon",
+    items: [
+      {
+        label: "Github",
+        href: "https://github.com/",
+      },
+      {
+        label: "Docs",
+        href: "https://gitbook.io/",
+      },
+      {
+        label: "Blog",
+        href: "https://medium.com/",
+      },
+    ],
+  },
+];
+
+export const socials = [
+  {
+    label: "Telegram",
+    icon: "TelegramIcon",
+    items: [
+      
+      {
+        label: "English",
+        href: "https://t.me/",
+      },
+    ],
+  },
+  {
+    label: "Twitter",
+    icon: "TwitterIcon",
+    href: "https://twitter.com/",
+  },
+];
+
+export const MENU_HEIGHT = 64;
+export const MENU_ENTRY_HEIGHT = 48;
+export const SIDEBAR_WIDTH_FULL = 240;
+export const SIDEBAR_WIDTH_REDUCED = 56;
